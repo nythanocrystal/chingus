02b22c6d770d8f491f4d0158e8930bb95c4870fe
nythanocrystal <valeriatails@gmail.com>
2022-05-12 10:08:17 -0400

    java is still for nerds

--- Mingus.py
+++ Mingus.py
@@ -1,20 +1,7 @@
 #Welcome to life
 import requests
-request = requests.get('http://api.open-notify.org')
-print(request.text)
-print("Hello")
-def bagels():
-    bagel = input("Would you like bagels?").lower()
-    if bagel == "yes":
-        print("BAGELS!!!!")
-    elif bagel == "no":
-        print(":(")
-    else:
-        print("what?")
 
-bagels()
-yee = "Bingus"
 
-for i in range(4):
-    yee += "!"
-    print(yee)
+for i in range(5):
+    request = requests.get('https://catfact.ninja/fact').json()
+    print(request['fact'])
